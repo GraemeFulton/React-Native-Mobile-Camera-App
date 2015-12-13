63a4702c5424825efa290d2be21b2af7b530b06a
Graeme Fulton <graemefulton89@gmail.com>
2015-12-13 19:46:11 +0000

    template with camera

---
+++ SoundCamera/ReactApp/screens/main-tabs.ios.js
@@ -0,0 +1,100 @@
+/**
+ * Tabbar
+ *
+ * React Native Starter App
+ * https://github.com/mcnamee/react-native-starter-app
+ */
+'use strict';
+
+/* ==============================
+  Initialise Component
+  =============================== */
+  // React
+  var React = require('react-native');
+
+  // App Globals
+  var AppStyles = require('../styles.ios');
+  var AppConfig = require('../config.ios');
+
+  // Screens / Pages
+  var ComingSoon = require('./soon.ios');
+
+  var {
+    StyleSheet,
+    TabBarIOS,
+    Component,
+  } = React;
+
+var TabBarItemIOS = TabBarIOS.Item;
+
+/* ==============================
+  View
+  =============================== */
+  class Tabbar extends Component {
+
+    /**
+      * Setup Default State Values
+      */
+    constructor(props) {
+      super(props);
+      this.state = {
+        selectedTab: 'favourites'
+      }
+    }
+    /**
+      * RENDER
+      */
+    render() {
+      return (
+        <TabBarIOS selectedTab={this.state.selectedTab}>
+          <TabBarItemIOS
+            selected={this.state.selectedTab === 'camera'}
+            title="Camera"
+            icon={require('../../images/icons/ios7-camera-outline.png')}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'camera',
+              });
+            }}>
+            <ComingSoon navigator={this.props.navigator} placeholder={"This could be whatever youd like..."} />
+          </TabBarItemIOS>
+
+          <TabBarItemIOS
+            selected={this.state.selectedTab === 'stream'}
+            title="Stream"
+            icon={require('../../images/icons/ios7-albums-outline.png')}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'stream',
+              });
+            }}>
+            <ComingSoon navigator={this.props.navigator} placeholder={"This could be a screen listing contacts..."} />
+          </TabBarItemIOS>
+
+          <TabBarItemIOS
+            selected={this.state.selectedTab === 'more'}
+            title="More"
+            icon={require('../../images/icons/ios7-more-outline.png')}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'more',
+              });
+            }}>
+            <ComingSoon navigator={this.props.navigator} placeholder={"This could be a settings screen..."} />
+          </TabBarItemIOS>
+
+        </TabBarIOS>
+      )
+    }
+  }
+
+/* ==============================
+  Styles
+  =============================== */
+  var styles = StyleSheet.create({
+  });
+
+/* ==============================
+  Done!
+  =============================== */
+  module.exports = Tabbar;
--- SoundCamera/index.ios.js
+++ SoundCamera/index.ios.js
@@ -1,53 +1,181 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * React Native Starter App
+ * https://github.com/mcnamee/react-native-starter-app
  */
 'use strict';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+/* ==============================
+  Initialise App
+  =============================== */
+  // React
+  var React = require('react-native');
+  var EventEmitter = require('EventEmitter');
+  var Subscribable = require('Subscribable');
 
-var SoundCamera = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+  // 3rd Party Components
+  var NavigationBar = require('react-native-navbar');
+  var SideMenu = require('react-native-side-menu');
+
+  // App Globals
+  var AppStyles = require('./ReactApp/styles.ios');
+
+  // Components
+  var Icons = require('./ReactApp/components/icons.ios');
+  var Menu = require('./ReactApp/components/menu.ios');
+
+  // Screens / Pages
+  var Index = require('./ReactApp/screens/main-tabs.ios');
+
+  var {
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Navigator,
+    Text,
+    View,
+  } = React;
+
+/* ==============================
+  Main Navigator with Sidemenu
+  =============================== */
+
+  /**
+   * Custom `Title` component
+   */
+  class CustomTitle extends React.Component {
+    render() {
+      return (
+        <Text style={[AppStyles.baseText, AppStyles.strong, AppStyles.navbar_title]}>{this.props.title}</Text>
+      );
+    }
   }
-});
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  /**
+   *  Main View w/ Sidebar
+   */
+  var SoundCamera = React.createClass({
+    mixins: [Subscribable.Mixin],
 
-AppRegistry.registerComponent('SoundCamera', () => SoundCamera);
+    /**
+      * Before Load
+      */
+    getInitialState: function() {
+      return {
+        touchToClose: true,
+        disableGestures: false,
+      };
+    },
+
+    /**
+      * On Load
+      */
+    componentWillMount: function() {
+      this.eventEmitter = new EventEmitter();
+    },
+
+    /**
+      * When Back Button from NavBar is Clicked
+      */
+    onLeftBackButtonPress: function(navigator) {
+      this.refs.rootNavigator.pop();
+    },
+
+    /**
+      * When Hamburger from NavBar is Clicked
+      */
+    onLeftButtonPress: function() {
+      this.eventEmitter.emit('toggleMenu');
+    },
+
+    /**
+      * Navigates to page from menu
+      */
+    navigate: function(title, link) {
+      this.refs.rootSidebarMenu.closeMenu();
+
+      this.refs.rootNavigator.replace({
+        title: title,
+        component: link,
+      });
+    },
+
+    /**
+      * Generate Custom Navbar
+      */
+    renderScene: function(route, navigator) {
+      var Component = route.component;
+      var navBar = route.navigationBar;
+
+      // Icons
+      var MenuIcon = Icons.MenuIcon;
+      var BackIcon = Icons.BackIcon;
+
+      // Navbar Setup
+      if (navBar) {
+        navBar = React.addons.cloneWithProps(navBar, {
+          navigator: navigator,
+          route: route
+        });
+      }
+
+      // Default Navbar Title
+      var title = 'Sound Camera';
+      if(route.title != undefined) {
+        title = route.title;
+      }
+
+      // Determine which Icon component - hamburger or back?
+      var customPrev = <MenuIcon leftButtonPress={this.onLeftButtonPress} />;
+      if (route.index > 0){
+        var customPrev = <BackIcon leftButtonPress={this.onLeftBackButtonPress} />;
+      }
+
+      // Done
+      return (
+        <View style={AppStyles.container}>
+          <NavigationBar
+            title={title}
+            style={AppStyles.navbar}
+            customPrev={customPrev}
+            customTitle={<CustomTitle title={title} />} />
+
+          <Component navigator={navigator} route={route} />
+        </View>
+      );
+    },
+
+    /**
+      * RENDER
+      */
+    render: function() {
+      return (
+        <SideMenu
+          ref="rootSidebarMenu"
+          menu={<Menu events={this.eventEmitter} navigate={this.navigate} />}
+          touchToClose={this.state.touchToClose}
+          disableGestures={this.state.disableGestures}>
+
+          <Navigator
+            ref="rootNavigator"
+            style={[AppStyles.container, AppStyles.appContainer]}
+            renderScene={this.renderScene}
+            initialRoute={{
+              component: Index,
+              index: 0,
+            }} />
+
+        </SideMenu>
+      );
+    }
+  });
+
+/* ==============================
+  Styles
+  =============================== */
+  var styles = StyleSheet.create({
+  });
+
+/* ==============================
+  Done!
+  =============================== */
+  AppRegistry.registerComponent('SoundCamera', () => SoundCamera);
